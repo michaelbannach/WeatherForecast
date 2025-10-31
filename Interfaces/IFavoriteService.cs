151fc9e56be60666be8b431a31a936c664b46ece
michaelbannach <michaelbannach@gmx.de>
2025-10-31 19:27:32 +0100

    IFavoriteService programmiert

--- Interfaces/IFavoriteService.cs
+++ Interfaces/IFavoriteService.cs
@@ -1,6 +1,12 @@
+using WeatherForecast.Models;
+
+
 namespace WeatherForecast.Interfaces;
 
-public class IFavoriteService
+public interface IFavoriteService
 {
+    Task<List<Favorite>> GetFavoritesAsync(string userId);
+    Task <bool> AddAsync(Favorite favorite);
     
+    Task<bool>DeleteByIdAsync(string userId, int id);
 }
