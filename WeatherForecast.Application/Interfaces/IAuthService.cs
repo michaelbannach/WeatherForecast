65b810cdef9efe2766faf3200d8659fa3140a8a7
michaelbannach <michaelbannach@gmx.de>
2025-12-02 13:15:38 +0100

    Add IAutherService Interface in Application Layer

--- WeatherForecast.Application/Interfaces/IAuthService.cs
+++ WeatherForecast.Application/Interfaces/IAuthService.cs
@@ -1,6 +1,10 @@
 namespace WeatherForecast.Application.Interfaces;
 
-public class IAuthService
+public interface IAuthService
 {
+    Task<(bool success, string? error)> LoginAsync(string email, string password);
     
+    Task<(bool success, string? error, string? appUserId, int? userId)> RegisterAsync(string email, string password, string firstName, string lastName, string role);
+    
+    Task LogoutAsync();
 }
